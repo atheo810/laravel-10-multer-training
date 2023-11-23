e4aa438aea1b72a01062c58d7e369a2dffbfdc66
atheo810 <atheos810@gmail.com>
2023-11-24 01:22:54 +0800

    feat: ImageController

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -8,6 +8,15 @@ class ImageController extends Controller
 {
     public function handleImage(Request $request)
     {
+        $request->validate([
+            'image' => ['required', 'min:100', 'max:1500', 'mimes:png,jpg,gif']
+        ]);
+        $request->image->storeAs('/images', 'new_images.png');
         return $request->all();
+    }
+
+    public function download()
+    {
+        return response()->download(public_path('/storage/images/new_images.png'));
     }
 }
