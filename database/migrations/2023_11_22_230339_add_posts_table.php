4e1b7cd7179916ef922212b1979feeff014b5bbf
atheo810 <atheos810@gmail.com>
2023-11-23 19:22:02 +0800

    feat: Category ID

--- database/migrations/2023_11_22_230339_add_posts_table.php
+++ database/migrations/2023_11_22_230339_add_posts_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::table('posts', function (Blueprint $table) {
             $table->date('publish_date');
             $table->integer('user_id');
+            $table->integer('category_id');
         });
     }
 
