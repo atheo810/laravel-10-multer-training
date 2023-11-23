560e60f19217fe78e1113768abf665d64989510f
atheo810 <atheos810@gmail.com>
2023-11-23 19:26:25 +0800

    feat: Post_tagFactory

---
+++ database/factories/Post_tagFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post_tag>
+ */
+class Post_tagFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'post_id' => rand(1, 10),
+            'tag_id' => rand(1, 10)
+        ];
+    }
+}
