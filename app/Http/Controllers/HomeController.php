cbb0667057e556d92af86537ef8f72372e195d74
atheo810 <atheos810@gmail.com>
2023-11-24 01:22:42 +0800

    feat: homeblade

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,15 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Address;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Post_tag;
+use App\Models\Tag;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $users =  User::all();
+        // return Post::with('tags')->first();
 
-        return view('home', compact('users'));
+        return view('home');
     }
 }
